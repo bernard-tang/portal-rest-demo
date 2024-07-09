17f4cf988730d245a1771fd7a27ed60286ff0b97
bernard <bernarddeng@hotmail.com>
2024-07-09 15:35:46 +0800

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom"
 import "./Navbar.css"
+import { useAuth } from "../utils/authContext";
+import { useNavigate } from 'react-router-dom';
 
 export function Navbar() {
+    const { logout } = useAuth(); // Destructure logout function from useAuth hook
+
+    const navigate = useNavigate();
+
+    const handleLogout = async (event) => {
+        const loggedout = await logout();
+
+        if(loggedout) {
+            navigate('/Login'); 
+        }
+
+    }
     return (
         <div className="navbar">
-            <Link to="/"><button className="navbutton">Home</button></Link>
-            <Link to="/Login"><button className="navbutton">Login</button></Link>
+            <Link to="/Home"><button className="navbutton">Home</button></Link>
+            {/* <Link to="/"><button className="navbutton">Login</button></Link> */}
             <Link to="/Products"><button className="navbutton">Products</button></Link>
             <Link to="/About"><button className="navbutton">About Us</button></Link>
             <Link to="/Contact"><button className="navbutton">Contact Us</button></Link>
+            <button className="navbutton" onClick={handleLogout}>Logout</button>
         </div>
     )
 }
