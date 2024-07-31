2e6ebe0e7b4db76f6ca9ded57c47e4c992191f06
Ber <bernarddeng@hotmail.com>
2024-07-31 11:46:21 +0800

    wpi for table pagination

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,46 @@
-export function Products() {
+import { useEffect, useState } from "react";
+import TableUnstyled from "../ui-component/tables/PaginationTable";
+import { getAllProducts } from "../services/ProductService";
+
+const Products = () => {
+    const [productData, setProductData] = useState(null);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [total, setTotal] = useState(1);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            const offset = page == 0 ? 0 : page * rowsPerPage;
+            const response = await getAllProducts(offset,rowsPerPage);
+            // const result = await response.entry.json();
+            setProductData(response.entry);
+            setTotal(response.total);
+        }
+
+        getProducts();
+    }, [ page, rowsPerPage ]);
+
+    const handlePageChange = (newPage) => {
+        setPage(newPage);
+      };
+    
+    const handleRowsChange = (newLimit) => {
+        setRowsPerPage(newLimit);
+    };
+
     return (
         <>
             <h1>Products Page</h1>
+            <TableUnstyled 
+            data={productData} 
+            page={page} 
+            rowsPerPage={rowsPerPage}
+            total={total}
+            onOffsetChange={handlePageChange} 
+            onLimitChange={handleRowsChange}
+            ></TableUnstyled>
         </>
     )
 }
+
+export default Products;
